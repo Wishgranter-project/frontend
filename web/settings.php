149c940de39bc8a98867588dcae83e59fd0ecfda
Adinan Cenci <adinancenci@protonmail.com>
2023-10-26 18:17:34 -0300

    updated the settings.php

--- web/settings.php
+++ web/settings.php
@@ -33,35 +33,6 @@ function baseHref(string $protocol, string $host, string $path, string $port) : 
     return $baseUrl;
 }
 
-function ping(string $url) : bool
-{
-    $port = preg_match('/:([\d]+)/', $url, $matches)
-        ? $matches[1]
-        : 80;
-
-    $url = preg_replace('/:\d+/', '', $url);
-
-    $ch = curl_init($url);
-    curl_setopt($ch, CURLOPT_NOBODY, true);
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-    curl_setopt($ch, CURLOPT_PORT, $port);
-    curl_exec($ch);
-    $retcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-    curl_close($ch);
-    return $retcode == 200 || $retcode == 0;
-}
-
-function cycleBackEndPort() : string
-{
-    if (!isset($_SESSION['backEndPorts']) || empty($_SESSION['backEndPorts'])) {
-        $_SESSION['backEndPorts'] = ['8080', '8000', '80'];
-    }
-
-    $first = array_shift($_SESSION['backEndPorts']);
-
-    return $first;
-}
-
 //------------------------------------------------------------------------
 
 if (isLocalEnvironment()) {
@@ -74,9 +45,7 @@ if (isLocalEnvironment()) {
     $backendProtocol = 'http';
     $backendHost     = 'player-backend.lndo.site';
     $backendPath     = '/';
-    $backendPort     = !empty($_SESSION['sucessfullyContactedBackEnd'])
-                            ? $_SESSION['sucessfullyContactedBackEnd']
-                            : cycleBackEndPort();
+    $backendPort     = '80';
 
 } else {
 
@@ -92,18 +61,6 @@ if (isLocalEnvironment()) {
 
 }
 
-if (isset($_GET['refresh'])) {
-    unset($_SESSION['sucessfullyContactedBackEnd']);
-}
-
 $selfBaseHref        = baseHref($selfProtocol, $selfHost, $selfPath, $selfPort);
 $backendBaseHref     = baseHref($backendProtocol, $backendHost, $backendPath, $backendPort);
-
-if (isLocalEnvironment() && empty($_SESSION['sucessfullyContactedBackEnd'])) {
-    if (ping($backendBaseHref)) {
-        $_SESSION['sucessfullyContactedBackEnd'] = $backendPort;
-    } else {
-        header('Location: ' . $selfBaseHref);
-    }
-}
 
