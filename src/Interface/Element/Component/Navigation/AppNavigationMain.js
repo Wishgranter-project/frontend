3ba1e09fcb8358e321b2c15e79e04b5ca5a9d04d
Adinan Cenci <adinancenci@protonmail.com>
2026-08-05 21:58:17 +0100

    minor improvement

--- src/Interface/Element/Component/Navigation/AppNavigationMain.js
+++ src/Interface/Element/Component/Navigation/AppNavigationMain.js
@@ -33,8 +33,8 @@ class AppNavigationMain extends CustomElement
         this.classList.add('app-navigation__main');
 
         this.createAndAttach('a', {href: '#home', title: 'Homepage', class: 'btn'}, this.create('span', {class: 'fa fa-home'}));
-        this.createAndAttach('a', {href: '#home', title: 'Search', class: 'btn'}, this.create('span', {class: 'fa fa-search'}));
-        this.createAndAttach('a', {href: '#home', title: 'Discover', class: 'btn'}, this.create('span', {class: 'fa fa-search-plus'}));
+        this.createAndAttach('a', {href: `#user:${this.api.defaultUserId}/search`, title: 'Search', class: 'btn'}, this.create('span', {class: 'fa fa-search'}));
+        this.createAndAttach('a', {href: '#discover:artist', title: 'Discover', class: 'btn'}, this.create('span', {class: 'fa fa-search-plus'}));
 
         this.$refs.createPlaylist    = this.createAndAttach('button', {title: 'Create playlist'}, this.create('span', {class: 'fa fa-plus-circle'}));
         this.$refs.downloadPlaylists = this.createAndAttach('button', {title: 'Download entire collection'}, this.create('span', {class: 'fa fa-download'}));
